82024b08db77496fd1e3dee3f3cf26b462cf1cf7
sobitov2000 <90320800+sobitov2000@users.noreply.github.com>
2021-11-18 17:25:43 +0500

    meni javascriptda qilganim

---
+++ java dars_1/js/main.js
@@ -0,0 +1,39 @@
+// console.log(5+25**2) < (10-5**3);
+// JavaScript Syntax
+// console.log("salom");
+// console.log("aleykum");
+// Variables
+// var firstNumber =2
+// var secondNumber =3
+// firstNumber =5
+// console.log(firstNumber + secondNumber);
+
+// //Cases
+// snake_case = first_number
+// PascalCase = FirstNumber
+// camelCase =firstNumber
+// Data Types Number, String, Boolean
+
+//Number
+// var firstNumber =3.4
+// console.log(typeof firstNumber, firstNumber);
+// console.log(typeof 3);
+// Math operators
+// console.log(10*3);
+// console.log(44 + 22 * 2);
+// console.log(11 % 3);
+//  String
+// var name="ahmad"
+// var age = 24
+// console.log(name, age);
+// var myName = prompt ("Ismingiz yozing")
+// var myAge = prompt ("Yoshingizni kiriting")
+// console.log(myName, myAge);
+
+var age="18 "+5
+console.log(age +10);
+//Vazifa
+var myBudget =1000 //USD
+var myExpense
+//Manda so'rashi kerak qancha rasxod qildingiz deb kiritsam va mani byudjetimga qanca pulim qolganni ko'rsatishi kerak.
+prompt
